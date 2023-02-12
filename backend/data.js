d774aa8f1977e6b823ee289f8d6c7b4140338758
Adela3003 <79999674+Adela3003@users.noreply.github.com>
2023-02-12 16:33:45 +0200

    k

---
+++ backend/data.js
@@ -0,0 +1,62 @@
+//import bcrypt from 'bcryptjs';
+const data = {
+    coaches: [
+      {
+        //id: 1,
+        name: 'Popescu Daniela',
+        image: '/images/PopescuDaniela.jpg',
+        age: 36,
+        description: 'trainer for beginner players',
+      },
+      {
+        //id: 2,
+        name: 'Stroe Bogdan',
+        image: '/images/StroeBogdan.jpg',
+        age: 35,
+        description: 'trainer for beginner players',
+      },
+      {
+        //id: 3,
+        name: 'Georgescu Ioana',
+        image: '/images/GeorgescuIoana.jpg',
+        age: 40,
+        description: 'coach for middle level players',
+      },
+      {
+        //id: 4,
+        name: 'Toma Alexandru',
+        image: '/images/TomaAlexandru.jpeg',
+        age: 43,
+        description: 'coach for middle level players',
+      },
+      {
+        //id: 5,
+        name: 'Davidescu Anastasia',
+        image: '/images/DavidescuAnastasia.jpg',
+        age: 37,
+        description: 'coach for professional players',
+      },
+      {
+        //id: 6,
+        name: 'Paraschiv Luca',
+        image: '/images/ParaschivLuca.jpg',
+        age: 44,
+        description: 'coach for professional players',
+      },
+    ],
+    // users: [
+    //   {
+    //      name: 'Adela',
+    //      email: 'adela@example.com',
+    //      password: bcrypt.hashSync('123456'),
+    //      isAdmin: true,
+    //   },
+    //   {
+    //      name: 'John',
+    //      email: 'john@example.com',
+    //      password: bcrypt.hashSync('12345'),
+    //      isAdmin: false,
+    //   }
+    // ],
+  };
+  export default data;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-import express from express;
+import data from "./data.js";
+import express from 'express';
+
+const app = express();
+
+app.get('/api/coaches', (req, res) => {
+    res.send(data.coaches)
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
