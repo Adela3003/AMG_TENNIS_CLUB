12fe3f147d46358a8cfc8e09a76b9f62a4302a93
Adela3003 <79999674+Adela3003@users.noreply.github.com>
2023-02-12 17:12:24 +0200

    db

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
 import data from "./data.js";
 import express from 'express';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then( ()=> {
+     console.log("Connected to db");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
 
