1400fb036403809735af2696890a703d6922e97a
Adela3003 <79999674+Adela3003@users.noreply.github.com>
2023-02-12 16:09:48 +0200

    re

--- frontend/src/containers/Register/Register.js
+++ frontend/src/containers/Register/Register.js
@@ -22,7 +22,7 @@ export const Register = (props) => {
             <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
             <label htmlFor="password">password</label>
             <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <button type="submit">Log In</button>
+            <button type="submit">Register</button>
           </form>
         </div>
         </div>
